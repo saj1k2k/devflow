b2b59c214b5c3b3b20524460d9ed1961af5f335b
saj1k2k <jonikbarezzi3@gmail.com>
2023-11-13 14:56:54 +0300

    add auth with clerk

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">We are here</h1>
-      <h1 className="h2-bold">We are here</h1>
-      <h1 className="h3-bold">We are here</h1>
-    </div>
-  );
-}
