628e0081e005e5b6664dfa0dc10e5f0d34678376
saj1k2k <jonikbarezzi3@gmail.com>
2023-12-15 16:09:51 +0300

    done global search

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Image from "next/image";
 import {Input} from "@/components/ui/input";
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
@@ -12,10 +12,27 @@ const GlobalSearch = () => {
     const pathname = usePathname();
     const searchParams = useSearchParams();
     const query = searchParams.get('q')
-
+    const searchContainerRef = useRef(null)
     const [search, setSearch] = useState(query || '')
     const [isOpen, setIsOpen] = useState(false)
 
+    useEffect(() => {
+        const handleOutsideClick = (event: any ) => {
+            if(searchContainerRef.current &&
+                // @ts-ignore
+                !searchContainerRef.current.contains(event.target)
+            ) {
+                setIsOpen(false);
+                setSearch('')
+            }
+        }
+        setIsOpen(false)
+        document.addEventListener('click', handleOutsideClick)
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick)
+        }
+    }, [pathname])
 
     useEffect(() => {
         const delayDebounceFn = setTimeout(() => {
@@ -45,7 +62,7 @@ const GlobalSearch = () => {
 
 
     return (
-        <div className='relative w-full max-w-[600px] max-lg:hidden'>
+        <div ref={searchContainerRef} className='relative w-full max-w-[600px] max-lg:hidden'>
             <div className='background-light800_darkgradient relative flex
             min-h-[56px] grow items-center gap-1 rounded-xl px-4'>
                 <Image src='/assets/icons/search.svg' alt='search'
